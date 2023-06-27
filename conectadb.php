1f758056e40f6e0a038b661ed5d8652749fdb2cc
braian-og7 <braian.pgoulart@senacsp.edu.br>
2023-06-27 11:48:12 -0300

    Tela de cadastro de clientes

--- conectadb.php
+++ conectadb.php
@@ -1,17 +1,17 @@
 <?php
-## ARQUIVO DE ACESSO AO BANCO DE DADOS ##
-## ALERTA EM MODO PROFISSIONAL ARQUIVO DEVE-SE MANTER OCULTO E PROTEGIDO ##
+  ## ARQUIVO DE ACESSO AO BANCO DE DADOS ##
+    ## ALERTA EM MODO PROFISSIONAL ARQUIVO DEVE-SE MANTER OCULTO E PROTEGIDO ##
 
-## LOCALIZA O PC OU SERVIDOR COM O BANCO (NOME DO COMPUTADOR)
-$servidor = "localhost";
-## NOME DO BANCO
-$banco = "projeto22";
-## USUARIO DE ACESSO
-$usuario = "administrador";
-## SENHA DO USUARIO
-$senha = "";
+    ## LOCALIZA O PC OU SERVIDOR COM O BANCO (NOME DO COMPUTADOR)
+    $servidor = "localhost";
+    ## NOME DO BANCO
+    $banco = "projeto22";
+    ##USUARIO DE ACESSO
+    $usuario = "administrador";
+    ##SENHA DO USUARIO
+    $senha = "123";
 
-#LINK DE CONEXÃO
-$link = mysqli_connect($servidor, $usuario, $senha, $banco);
+    #LINK DE CONEXÃO
+    $link = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 ?>
