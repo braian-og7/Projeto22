5923ea422d70e3fac298df0ff39ff8f1292b6ad0
braian-og7 <braian.pgoulart@senacsp.edu.br>
2023-07-24 11:45:26 -0300

    Updates

--- listaproduto.php
+++ listaproduto.php
@@ -1,75 +1,58 @@
 <?php
 include("conectadb.php");
-#A SESSÃO DO USUARIO DO ADMINISTRADOR
-
- 
 
 session_start();
-$nomeusuario = $_SESSION["nomeusuario"];
+$nomeusuario = $_SESSION['nomeusuario'];
 
- 
-
-#JÁ LISTA OS USUARIOS DO MEU BANCO
-$sql = "SELECT * FROM usuarios WHERE usu_ativo = 's'";
+$sql = "SELECT * FROM produtos WHERE pro_ativo = 's'";
 $retorno = mysqli_query($link, $sql);
+$ativo = "s";
 
- 
-
-#JÁ FORÇA TRAZER s NA VARIÁVEL ATIVO
-$ativo = 's';
-
- 
-
-#COLETA O BOTÃO DE POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $ativo = $_POST['ativo'];
 
- 
-
-    #VERIFICA SE USUARIO ESTÁ ATIVO PARA LISTAR
-    if ($ativo == 's') {
-        $sql = "SELECT * FROM usuarios WHERE usu_ativo = 's' ";
+    #VALIDA SE PRODUTO EXISTE
+    if($ativo == 's'){
+        $sql = "SELECT * FROM produtos WHERE pro_ativo = 's'";
         $retorno = mysqli_query($link, $sql);
-    } else {
-        $sql = "SELECT * FROM usuarios WHERE usu_ativo = 'n' ";
+    }
+    else{
+        $sql = "SELECT * FROM produtos WHERE pro_ativo = 'n'";
         $retorno = mysqli_query($link, $sql);
     }
 }
-?>
 
- 
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
- 
-
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="./css/estiloadm.css">
-<title>LISTA USUARIOS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estiloadm.css">
+    <title>LISTA PRODUTOS</title>
 </head>
 
- 
-
 <body>
-<div>
-<ul class="menu">
-<li><a href="cadastrausuario.php">CADASTRA USUARIO</a></li>
-<li><a href="listausuario.php">LISTA USUARIO</a></li>
-<li><a href="cadastraproduto.php">CADASTRA PRODUTO</a></li>
-<li><a href="listaproduto.php">LISTA PRODUTO</a></li>
-<li><a href="listacliente.php">LISTA CLIENTE</a></li>
-<li class="menuloja"><a href="logout.php">SAIR</a></li>
-<?php
+    <!-- NOSSO MENU GLOBAL -->
+    <div>
+        <ul class="menu">
+            <li><a href="cadastrausuario.php">CADASTRA USUARIO</a></li>
+            <li><a href="listausuario.php">LISTA USUARIO</a></li>
+            <li><a href="cadastraproduto.php">CADASTRA PRODUTO</a></li>
+            <li><a href="listaproduto.php">LISTA PRODUTO</a></li>
+            <li><a href="listacliente.php">LISTA CLIENTE</a></li>
+            <li class="menuloja"><a href="logout.php">SAIR</a></li>
+            <?php
             #ABERTO O PHP PARA VALIDAR SE A SESSÃO DO USUARIO ESTÁ ABERTA
             #SE SESSÃO ABERTA, FECHA O PHP PARA USAR ELEMENTOS HTML
             if ($nomeusuario != null) {
             ?>
-<!-- USO DO ELEMENTO HTML COM PHP INTERNO -->
-<li class="profile">OLÁ <?= strtoupper($nomeusuario) ?></li>
-<?php
+                <!-- USO DO ELEMENTO HTML COM PHP INTERNO -->
+                <li class="profile">OLÁ <?= strtoupper($nomeusuario) ?></li>
+            <?php
                 #ABERTURA DE OUTRO PHP PARA CASO FALSE
             } else {
                 echo "<script>window.alert('USUARIO NÃO AUTENTICADO');
@@ -77,56 +60,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
             #FIM DO PHP PARA CONTINUAR MEU HTML
             ?>
-</ul>
-</div>
+        </ul>
+    </div>
+    <!-- FIM MENU GLOBAL -->
 
- 
+    <div>
+        <form action="listaproduto.php" method="post">
+            <input type="radio" name="ativo" class="radio" value="s" required onclick="submit()" <?= $ativo == 's' ? "checked" : "" ?>>ATIVOS
 
-    <!-- AQUI LISTA OS USUARIOS DO BANCO  -->
-<div id="background">
-<form action="listausuario.php" method="post">
-<input type="radio" name="ativo" class="radio" value="s" required 
-            onclick="submit()" <?=$ativo =='s'?"checked":""?>>ATIVOS
-
- 
-
-            <input type="radio" name="ativo" class="radio" value="n" required 
-            onclick="submit()" <?=$ativo =='n'?"checked":""?>>INATIVOS
-</form>
-
- 
-
+            <input type="radio" name="ativo" class="radio" value="n" required onclick="submit()" <?= $ativo == 'n' ? "checked" : "" ?>>INATIVOS
+        </form>
         <div class="container">
-<table border="1">
-<tr>
-<th>NOME</th>
-<th>ALTERAR DADOS</th>
-<th>ATIVO?</th>
-</tr>
-<!-- BRUXARIA EM PHP -->
-<?php
+            <table border="1">
+                <tr>
+                    <th>ID PRODUTO</th>
+                    <th>NOME</th>
+                    <th>DESCRIÇÃO</th>
+                    <th>QUANTIDADE ESTOQUE</th>
+                    <th>CUSTO</th>
+                    <th>PREÇO</th>
+                    <th>IMAGEM</th>
+                    <th>ALTERAR</th>
+                    <th>ATIVO?</th>
+                </tr>
+                <?php
                     while($tbl = mysqli_fetch_array($retorno)){
-                ?>
-<tr>
-<td><?= $tbl[1]?></td> <!-- TRAZ SOMENTE A COLUNA 1 DO BANCO [NOME]-->
-<td><a href="alterausuario.php?id=<?= $tbl[0]?>">
-<input type="button" value="ALTERAR DADOS"></a></td> <!-- CRIANDO UM BOTÃO ALTERAR PASSANDO O ID DO USUARIO NA URL VIA GET -->
-<td><?=$check =($tbl[3] == 's')?"SIM":"NÃO"?></td> <!-- VALIDA S OU N E ESCREVE "SIM" E "NÃO"-->
-</tr>
-<?php
+                        ?>
+                    
+                    <tr>
+                        <td><?=$tbl[0]?></td> <!-- COLETA ID PRODUTO COLUNA 0-->
+                        <td><?=$tbl[1]?></td> <!-- COLETA NOME PRODUTO COLUNA 1-->
+                        <td><?=$tbl[2]?></td> <!-- COLETA DESCRICAO PRODUTO COLUNA 2-->
+                        <td><?=$tbl[3]?></td> <!-- COLETA QUANTIDADE PRODUTO COLUNA 3-->
+                        <!-- CONVERTE PONTO EM VIRGULA-->
+                        <td>R$ <?=number_format($tbl[4],2,',','.')?></td>
+                        <td>R$ <?=number_format($tbl[5],2,',','.')?></td>
+                        <!-- VEM DO BANCO A IBAGEM EM BASE64 O QUE FAZER?? -->
+                        <!-- DECRIPTAR O BASE64 TRAZENDO A IBAGEM -->
+                        <td><img src="data:image/jpeg;base64,<?=$tbl[7]?>" width="100" height="100"></td>
+                        <!-- BOTÃO ALTERAR PRODUTO -->
+                        <td>   
+                            <a href="alteraproduto.php?id=<?= $tbl[0]?>">
+                            <input type="button" value="ALTERAR">
+                        </td>
+                        <td><?= $check = ($tbl[6] == 's')?"SIM":"NÃO"?></td>
+                    </tr>
+                    <?php
                     }
-                    ?>
-</table>
-</div>
-
- 
-
+                    ?>21
+            </table>
+        </div>
     </div>
 
- 
-
 </body>
-
- 
 
 </html>
